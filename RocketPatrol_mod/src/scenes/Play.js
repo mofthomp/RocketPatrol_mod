f87d890c0aed85d60350df2d2a2d72b57aa31f76
mofthomp <45412519+mofthomp@users.noreply.github.com>
2021-04-14 12:26:32 -0500

    Update Play.js

--- RocketPatrol_mod/src/scenes/Play.js
+++ RocketPatrol_mod/src/scenes/Play.js
@@ -188,7 +188,7 @@ class Play extends Phaser.Scene{
 
         //score add and repaint
         this.p1Score += ship.points;
-        this.scoreLeft.text = 'Score:' + this.p1Score;
+        this.scoreLeft.text = 'Score: ' + this.p1Score;
 
         //essplode
         this.sound.play('sfx_explosion');
